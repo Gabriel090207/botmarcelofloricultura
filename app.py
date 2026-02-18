af96e41571d56021a4cdb67a8736979ba7f53648
Gabriel Borela <gabrielborela@MacBook-Air-de-Gabriel.local>
2026-02-18 18:34:46 -0300

    arrumando bug

--- app.py
+++ app.py
@@ -71,3 +71,7 @@ def webhook():
     except Exception as e:
         print("ERRO:", e)
         return jsonify({"status": "error"}), 500
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
