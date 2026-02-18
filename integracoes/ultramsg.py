b21292375ce6f30de7fc4119b127088e09998511
Gabriel Borela <gabrielborela@MacBook-Air-de-Gabriel.local>
2026-02-18 17:54:58 -0300

    sem botões por enquanto

--- integracoes/ultramsg.py
+++ integracoes/ultramsg.py
@@ -1,6 +1,5 @@
 import requests
 import os
-import json
 
 
 ULTRAMSG_INSTANCE = os.environ.get("ULTRAMSG_INSTANCE")
@@ -18,26 +17,4 @@ def enviar_mensagem(numero, mensagem):
 
     response = requests.post(url, data=payload)
     print("RESPOSTA CHAT:", response.text)
-    return response.text
-
-
-def enviar_botoes(numero, titulo, texto, botoes):
-    """
-    Envio de botão no formato novo da UltraMsg
-    """
-
-    url = f"https://api.ultramsg.com/{ULTRAMSG_INSTANCE}/messages/interactive"
-
-    payload = {
-        "token": ULTRAMSG_TOKEN,
-        "to": numero,
-        "type": "button",
-        "header": titulo,
-        "body": texto,
-        "footer": "",
-        "buttons": json.dumps(botoes)
-    }
-
-    response = requests.post(url, data=payload)
-    print("RESPOSTA INTERACTIVE:", response.text)
     return response.text
